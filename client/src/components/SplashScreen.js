21dca416251e75d5a604cfc25f4f28374ec36563
Zian Shang <zian.shang@stonybrook.edu>
2022-11-28 23:53:36 -0500

    splash screen update

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,36 @@
+import myImage from "./logo.png";
+import Button from '@mui/material/Button';
+import { useHistory } from 'react-router-dom'
+import Typography from '@mui/material/Typography';
+
 export default function SplashScreen() {
+    const history = useHistory();
     return (
         <div id="splash-screen">
-            Playlister
+            <b className="welcome">Welcome to</b>
+            <img className="splash-screen-logo" src={myImage} alt="playlister logo" />
+            <Typography id="splash-screen-discription">an application for creating and playing playlists of YouTube music videos</Typography>
+            <div className="splash-screen-buttons">
+                <Button
+                    id='login-button'
+                    onClick={() => history.push('/login')}
+                    variant="contained">
+                        Login
+                </Button>
+                <Button
+                    id='create-account-button'
+                    onClick={() => history.push('/register')}
+                    variant="contained">
+                        Create New Account
+                </Button>
+                <Button
+                    id='guest-button'
+                    // onClick={handleGuestContinue}
+                    variant="contained">
+                    Continue as guest
+                </Button>
+            </div>
+            
         </div>
     )
 }

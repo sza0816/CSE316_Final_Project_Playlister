b6d4925000bf87e71c9c147ac34243f7f8e76caf
Zian Shang <zian.shang@stonybrook.edu>
2022-11-29 00:20:18 -0500

    login screen adjustment

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -16,6 +16,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import { width } from '@mui/system';
 
 export default function LoginScreen() {
     const { auth } = useContext(AuthContext);
@@ -31,23 +32,9 @@ export default function LoginScreen() {
     };
 
     return (
-        <Grid container component="main" sx={{ height: '100vh' }}>
+        <Grid container component="main" sx={{ height: '100vh'}}>
             <CssBaseline />
-            <Grid
-                item
-                xs={false}
-                sm={4}
-                md={7}
-                sx={{
-                    backgroundImage: 'url(https://source.unsplash.com/random)',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundColor: (t) =>
-                        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                }}
-            />
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Grid sx={{width:"100%"}} component={Paper} elevation={6} square>
                 <Box
                     sx={{
                         my: 8,
@@ -55,7 +42,7 @@ export default function LoginScreen() {
                         display: 'flex',
                         flexDirection: 'column',
                         alignItems: 'center',
-                    }}
+                      }}
                 >
                     <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
                         <LockOutlinedIcon />
